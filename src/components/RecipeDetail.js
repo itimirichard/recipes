e013739846da97a2b9ad93d5a3ca2c68ad355c36
richie <richard.itimi@interswitchgroup.com>
2018-09-03 10:15:00 +0100

    added react and components, addeded setup .env

---
+++ src/components/RecipeDetail.js
@@ -0,0 +1,24 @@
+import React from 'react';
+
+const RecipeDetail = (props) => (
+  <div style={props.style}>
+    <h2>Creepy Halloween Skull Cupcakes</h2>
+    <img src={''} />
+    <div>
+      <span>Dessert</span>
+      <span>1200 cal</span>
+    </div>
+    <h3>Ingredients</h3>
+    <ul>
+      <li>1 package devil's food cake mix</li>
+      <li>1 Cup of water</li>
+      <li>3 eggs</li>
+    </ul>
+    <h3>Steps</h3>
+    <ol>
+      <li></li>
+    </ol>
+  </div>
+);
+
+export default RecipeDetail
--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,35 @@
+const path = require('path');
 const webpack = require('webpack');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+require('dotenv').config();
 
 module.exports = {
-  entry: './index.js',
+  context: path.join(__dirname, 'src'),
+  entry: ['whatwg-fetch', './index.js'],
   output: {
-    path: __dirname,
-    filename: './bundle.js'
-  }
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'bundle.js'
+  },
+  module: {
+    rules: [
+      {
+        test: /\.jsx?$/,
+        loader: 'babel-loader'
+      },
+      {
+        test: /\.(png|jpeg)$/,
+        loader: 'file-loader'
+      }
+    ],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: 'index.html',
+      inject: 'body',
+    }),
+    new webpack.DefinePlugin({
+      API_URL: JSON.stringify(process.env.API_URL)
+    })
+  ]
 };
