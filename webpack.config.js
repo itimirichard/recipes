2dad594ad00f6b2bebeda2ab8e281ed29a309e97
richie <richard.itimi@interswitchgroup.com>
2018-09-04 11:37:10 +0100

    setup basscss

--- webpack.config.js
+++ webpack.config.js
@@ -15,7 +15,12 @@ module.exports = {
     rules: [
       {
         test: /\.jsx?$/,
+        exclude: /node_modules/,
         loader: 'babel-loader'
+      },
+      {
+        test: /\.css/,
+        loader: ['style-loader', 'css-loader', 'postcss-loader']
       },
       {
         test: /\.(png|jpeg)$/,
